975d98e094cf99bbf89c55456571bb25e34a528d
K-Hebs <diatta4@gmail.com>
2023-04-01 19:27:45 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,76 +1,55 @@
-function setClock(clockElement, hours, minutes) {
-    const hourHand = clockElement.querySelector('.hour-hand');
-    const minuteHand = clockElement.querySelector('.minute-hand');
+const clock1HourHand = document.querySelector("#clock1 .hour-hand");
+const clock1MinuteHand = document.querySelector("#clock1 .minute-hand");
+const clock2HourHand = document.querySelector("#clock2 .hour-hand");
+const clock2MinuteHand = document.querySelector("#clock2 .minute-hand");
+const hourInput = document.querySelector("#hour-input");
+const minuteInput = document.querySelector("#minute-input");
+const submitButton = document.querySelector("#submit-button");
+const hint = document.querySelector(".hint");
 
-    const hoursRotation = (hours % 12) / 12 * 360 + (minutes / 60) * 30;
-    const minutesRotation = (minutes / 60) * 360;
+let askedHour;
+let askedMinute;
 
-    hourHand.style.transform = `rotate(${hoursRotation}deg)`;
-    minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+function getCurrentRotation(element) {
+    const currentTransform = element.style.transform;
+    const regex = /rotate\(([\d.-]+)deg\)/;
+    const match = currentTransform.match(regex);
+    return match ? parseFloat(match[1]) : 0;
 }
 
-function generateRandomTime() {
-    const hours = Math.floor(Math.random() * 12);
-    const minutes = Math.floor(Math.random() * 60);
-    return { hours, minutes };
+function setCurrentTime(hourHand, minuteHand, hour, minute) {
+    const hourRotation = (hour % 12) * 30 + minute * 0.5;
+    const minuteRotation = minute * 6;
+    hourHand.style.transform = `rotate(${hourRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
 }
 
-function displayHint(hintElement, correctHours, correctMinutes, inputHours, inputMinutes) {
-    const hourDifference = Math.abs(correctHours - inputHours);
-    const minuteDifference = Math.abs(correctMinutes - inputMinutes);
+function getRandomTime() {
+    const hour = Math.floor(Math.random() * 12);
+    const minute = Math.floor(Math.random() * 6) * 10;
+    return { hour, minute };
+}
 
-    let hint = '';
+function setRandomTime() {
+    const { hour, minute } = getRandomTime();
+    setCurrentTime(clock1HourHand, clock1MinuteHand, hour, minute);
+    askedHour = hour;
+    askedMinute = minute;
+}
 
-    if (correctHours === inputHours && correctMinutes === inputMinutes) {
-        hint = 'Great job! You got it right!';
+function checkAnswer() {
+    const userHour = parseInt(hourInput.value);
+    const userMinute = parseInt(minuteInput.value);
+
+    if (userHour === askedHour && userMinute === askedMinute) {
+        hint.textContent = "Correct!";
     } else {
-        if (correctHours !== inputHours) {
-            if (correctHours > inputHours) {
-                hint += `The hour is too low. Try a higher number. `;
-            } else {
-                hint += `The hour is too high. Try a lower number. `;
-            }
-        }
-
-        if (correctMinutes !== inputMinutes) {
-            if (correctMinutes > inputMinutes) {
-                hint += `The minute is too low. Try a higher number.`;
-            } else {
-                hint += `The minute is too high. Try a lower number.`;
-            }
-        }
+        hint.textContent = `Not quite. The time is close to ${askedHour}:${askedMinute < 10 ? '0' + askedMinute : askedMinute}.`;
     }
 
-    hintElement.textContent = hint;
+    setCurrentTime(clock2HourHand, clock2MinuteHand, userHour, userMinute);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const clock1 = document.querySelector('.clock1');
-    const clock2 = document.querySelector('.clock2');
-    const submitTime1 = document.getElementById('submit-time1');
-    const submitTime2 = document.getElementById('submit-time2');
-    const hint1 = document.getElementById('hint1');
+submitButton.addEventListener("click", checkAnswer);
 
-    const randomTime1 = generateRandomTime();
-    const randomTime2 = generateRandomTime();
-    setClock(clock1, randomTime1.hours, randomTime1.minutes);
-    setClock(clock2, randomTime2.hours, randomTime2.minutes);
-
-    submitTime1.addEventListener('click', function () {
-        const hourInput1 = document.getElementById('hour-input1').value;
-        const minuteInput1 = document.getElementById('minute-input1').value;
-
-        if (hourInput1 && minuteInput1) {
-            displayHint(hint1, randomTime1.hours, randomTime1.minutes, parseInt(hourInput1), parseInt(minuteInput1));
-        }
-    });
-
-    submitTime2.addEventListener('click', function () {
-        const hourInput2 = document.getElementById('hour-input2').value;
-        const minuteInput2 = document.getElementById('minute-input2').value;
-
-        if (hourInput2 && minuteInput2) {
-            setClock(clock2, parseInt(hourInput2), parseInt(minuteInput2));
-        }
-    });
-});
+setRandomTime();
