87f25f47478865e63ca4fa3f10a9d5301ab19772
K-Hebs <diatta4@gmail.com>
2023-04-01 19:08:06 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -25,4 +25,52 @@ function displayHint(hintElement, correctHours, correctMinutes, inputHours, inpu
         hint = 'Great job! You got it right!';
     } else {
         if (correctHours !== inputHours) {
-            if (correctHours
+            if (correctHours > inputHours) {
+                hint += `The hour is too low. Try a higher number. `;
+            } else {
+                hint += `The hour is too high. Try a lower number. `;
+            }
+        }
+
+        if (correctMinutes !== inputMinutes) {
+            if (correctMinutes > inputMinutes) {
+                hint += `The minute is too low. Try a higher number.`;
+            } else {
+                hint += `The minute is too high. Try a lower number.`;
+            }
+        }
+    }
+
+    hintElement.textContent = hint;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const clock1 = document.querySelector('.clock1');
+    const clock2 = document.querySelector('.clock2');
+    const submitTime1 = document.getElementById('submit-time1');
+    const submitTime2 = document.getElementById('submit-time2');
+    const hint1 = document.getElementById('hint1');
+
+    const randomTime1 = generateRandomTime();
+    const randomTime2 = generateRandomTime();
+    setClock(clock1, randomTime1.hours, randomTime1.minutes);
+    setClock(clock2, randomTime2.hours, randomTime2.minutes);
+
+    submitTime1.addEventListener('click', function () {
+        const hourInput1 = document.getElementById('hour-input1').value;
+        const minuteInput1 = document.getElementById('minute-input1').value;
+
+        if (hourInput1 && minuteInput1) {
+            displayHint(hint1, randomTime1.hours, randomTime1.minutes, parseInt(hourInput1), parseInt(minuteInput1));
+        }
+    });
+
+    submitTime2.addEventListener('click', function () {
+        const hourInput2 = document.getElementById('hour-input2').value;
+        const minuteInput2 = document.getElementById('minute-input2').value;
+
+        if (hourInput2 && minuteInput2) {
+            setClock(clock2, parseInt(hourInput2), parseInt(minuteInput2));
+        }
+    });
+});
