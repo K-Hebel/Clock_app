3d8c4076a541c0384d20d6c98f9d62e1fa575708
K-Hebs <diatta4@gmail.com>
2023-04-01 16:09:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function setClock() {
+    const currentDate = new Date();
+    const seconds = currentDate.getSeconds();
+    const minutes = currentDate.getMinutes();
+    const hours = currentDate.getHours();
+
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    const secondsRotation = (seconds / 60) * 360;
+    const minutesRotation = (minutes / 60) * 360;
+    const hoursRotation = (hours / 12) * 360;
+
+    secondHand.style.transform = `rotate(${secondsRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+    hourHand.style.transform = `rotate(${hoursRotation}deg)`;
+}
+
+setInterval(setClock, 1000);
